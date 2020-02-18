97ce9ff3ab8c9aeff69accae4a2627ce79d1d5d4
João Fernandes <joao.fernandes@tecmic.pt>
2020-02-18 17:49:43 +0000

    First app commit

---
+++ XamFormsAPP/XamFormsAPP/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamFormsAPP.Models;
+
+namespace XamFormsAPP.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
